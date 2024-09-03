7fd176e0cfa584e1075bdd4808306c76a3ae6ad4
MSI GAMING <arthdika17@gmail.com>
2024-09-03 12:11:33 +0700

    Add Comment.kt

---
+++ src/main/kotlin/Comment.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("Hello, ini Program pertama saya di  kontlin")
+
+    //Contoh komentar  satu baris
+    /*
+    contoh komentar
+     */
+    /**
+     * kotlin Doc --> baris ini  untuk mendokumentasi kode program
+     */
+}
