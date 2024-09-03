90ebd0059c7e1c546d6355db9fbbf98f9bd907be
MSI GAMING <arthdika17@gmail.com>
2024-09-03 12:31:14 +0700

    Add valval.kt

---
+++ src/main/kotlin/valval.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    var varVariable: Int
+    varVariable = 56
+    println("Pemberian nilai Pertama pada varVariable = " +varVariable)
+    varVariable =78
+    println("Pemberian nilai kedua pada varVariable = " +varVariable)
+
+    val phi = 3.14
+
+}
