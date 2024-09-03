f5175c09e8ecffad0a6cda2dc4177812174f3149
MSI GAMING <arthdika17@gmail.com>
2024-09-03 12:00:04 +0700

    Add Literal.kt

--- src/main/kotlin/Literal.kt
+++ src/main/kotlin/Literal.kt
@@ -1,3 +1,13 @@
 fun main() {
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringLiteral = "Hello"
+    var boolLiteral = true
+
+    println("intLiteral = " + intLiteral)
+    println("doubleLiteral = " + doubleLiteral)
+    println("StringLiteral = " + stringLiteral)
+    println("boolLiteral = " + boolLiteral)
+
 
 }
