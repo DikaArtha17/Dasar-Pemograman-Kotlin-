03497f7e2e8a7bacfec7c004d0c38036f85eafe4
MSI GAMING <arthdika17@gmail.com>
2024-09-03 14:08:44 +0700

    Add constantsLiteral.kt

--- src/main/kotlin/floatingPoint.kt
+++ src/main/kotlin/floatingPoint.kt
@@ -2,6 +2,6 @@ fun main(args: Array<String>) {
     val maxFloat: Float = 9.123456789f
     val maxDouble: Double = 9.123456789
 
-    println("maxFloat = "+maxFloat)
-    println("maxDouble = "+maxDouble)
+    println("maxFloat = " + maxFloat)
+    println("maxDouble = " + maxDouble)
 }
