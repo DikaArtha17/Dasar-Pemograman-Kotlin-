3c71b9b840c15b507be59a811d4357bab05665b0
MSI GAMING <arthdika17@gmail.com>
2024-09-03 12:21:36 +0700

    Add variabel.kt

--- src/main/kotlin/variabel.kt
+++ src/main/kotlin/variabel.kt
@@ -1,4 +1,7 @@
 fun main() {
     var  dynamicVar = 2
     println("Contoh dynamic Variable = "+ dynamicVar)
+
+    var staticVar : Char = 'A'
+    println("Contoh  static Variable = " + staticVar)
 }
